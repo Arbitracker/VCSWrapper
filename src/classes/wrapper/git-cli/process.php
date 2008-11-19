61862e3d3c7791f9cf1e7be892d28f4263251f15
Kore Nordmann <kore@arbitracker.org>
2008-11-19 19:30:13 +0000

    - Implemented: Version check for installed GIT binary

--- src/classes/wrapper/git-cli/process.php
+++ src/classes/wrapper/git-cli/process.php
@@ -29,6 +29,14 @@
 class vcsGitCliProcess extends pbsSystemProcess
 {
     /**
+     * Static property containg information, if the version of the git CLI
+     * binary version has already been verified.
+     *
+     * @var bool
+     */
+    public static $checked = false;
+
+    /**
      * Class constructor taking the executable
      * 
      * @param string $executable Executable to create system process for;
@@ -40,6 +48,37 @@ class vcsGitCliProcess extends pbsSystemProcess
 
         $this->nonZeroExitCodeException = true;
         $this->argument( 'git' )->argument( '--no-pager' );
+    }
+
+    /**
+     * Verify git version
+     *
+     * Verify hat the version of the installed GIT binary is at least 1.6. Will
+     * throw an exception, if the binary is not available or too old.
+     * 
+     * @return void
+     */
+    protected static function checkVersion()
+    {
+        if ( self::$checked === true )
+        {
+            return true;
+        }
+
+        $process = new pbsSystemProcess();
+        $process->argument( 'git' )->argument( '--version' )->execute();
+
+        if ( !preg_match( '(\\d+(?:\.\\d+)+)', $process->stdoutOutput, $match ) )
+        {
+            throw new vcsRuntimeException( 'Could not determine GIT version.' );
+        }
+
+        if ( version_compare( $match[0], '1.6' ) )
+        {
+            return self::$checked = true;
+        }
+
+        throw new vcsRuntimeException( 'Git is required in a minimum version of 1.6.' );
     }
 }
 
